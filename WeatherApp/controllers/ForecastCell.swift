97f331dac1e221aa0f1b1509d963bdb0af6ca869
Adam Hajro <a.hajro@azet.sk>
2022-04-18 19:06:04 +0200

    Change in data models and cell of tableview.

--- WeatherApp/controllers/ForecastCell.swift
+++ WeatherApp/controllers/ForecastCell.swift
@@ -8,14 +8,6 @@
 import UIKit
 
 class ForecastCell: UITableViewCell {
-   
-    static let PERCENT = "%";
-    static let CELSIUS = "℃";
-    
-    static var classString : String {
-        String(describing: ForecastCell.self)
-    }
-    
     @IBOutlet weak var label_day: UILabel!
     @IBOutlet weak var img_typeOfWeather: UIImageView!
     @IBOutlet weak var label_humidity: UILabel!
@@ -23,33 +15,13 @@ class ForecastCell: UITableViewCell {
     
     func setWeather(forecastDay: ForecastDay)
     {
-        self.label_day.text = forecastDay.dayOfWeek
-        self.label_temperature.text = String(forecastDay.temperature) + ForecastCell.CELSIUS
-        self.img_typeOfWeather.image = getImage(type: forecastDay.typeOfWeather)
-        self.label_humidity.text = String(forecastDay.humidity) + ForecastCell.PERCENT
+        self.label_day.text = forecastDay.getDayOfWeek()
+        self.label_temperature.text = forecastDay.getTemperatureInCelsius()
+        self.img_typeOfWeather.image = forecastDay.getTypeOfWeather()
+        self.label_humidity.text = forecastDay.getHumidityInPercent()
     }
     
-    func getImage(type: ForecastDay.WeatherType) -> UIImage {
-        var img: UIImage = UIImage()
-        
-        switch type {
-        case .sunny:
-            img = UIImage(systemName: "sun.max.fill")!
-            break
-        case .cloudy:
-            img = UIImage(systemName: "cloud.fill")!
-            break
-        case .windy:
-            img = UIImage(systemName: "wind")!
-            break
-        case .rainy:
-            img = UIImage(systemName: "cloud.rain.fill")!
-            break
-        case .stormy:
-            img = UIImage(systemName: "cloud.bolt.fill")!
-            break
-        }
-        
-        return img
+    static var classString : String {
+        String(describing: ForecastCell.self)
     }
 }
--- WeatherApp/datamodels/ForecastDay.swift
+++ WeatherApp/datamodels/ForecastDay.swift
@@ -6,21 +6,54 @@
 //
 
 import Foundation
+import UIKit
 
 class ForecastDay {
     enum WeatherType {
       case sunny, cloudy, windy, rainy, stormy
     }
     
-    var dayOfWeek: String
-    var typeOfWeather: WeatherType
-    var humidity: Int
-    var temperature: Int
+    private var dayOfWeek: String
+    private var typeOfWeather: WeatherType
+    private var humidity: Int
+    private var temperature: Int
     
     init(dayOfWeek: String, typeOfWeather: WeatherType, humidity: Int, temperature: Int) {
         self.dayOfWeek = dayOfWeek
         self.typeOfWeather = typeOfWeather
         self.humidity = humidity
         self.temperature = temperature
+    }
+    
+    static let PERCENT = "%";
+    static let CELSIUS = "℃";
+    
+    //Atributy su zaenkapsulovane pre pripadne potreby uprav (daj v stupnoch celzia, ...)
+    func getDayOfWeek() -> String {
+        return dayOfWeek
+    }
+    
+    
+    func getTypeOfWeather() -> UIImage {
+        switch typeOfWeather {
+            case .sunny:
+                return UIImage(systemName: "sun.max.fill")!
+            case .cloudy:
+                return UIImage(systemName: "cloud.fill")!
+            case .windy:
+                return UIImage(systemName: "wind")!
+            case .rainy:
+                return UIImage(systemName: "cloud.rain.fill")!
+            case .stormy:
+                return UIImage(systemName: "cloud.bolt.fill")!
+        }
+    }
+    
+    func getHumidityInPercent() -> String{
+        return "\(humidity)" + ForecastDay.PERCENT
+    }
+    
+    func getTemperatureInCelsius() -> String {
+        return "\(temperature)" + ForecastDay.CELSIUS
     }
 }
